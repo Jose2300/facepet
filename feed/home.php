3d34f3808dd5ca613b252e315cf45b4ff1ff0a4e
Jose2300 <joitalomaciel@gmail.com>
2023-03-31 14:50:39 -0300

    commit

--- feed/home.php
+++ feed/home.php
@@ -3,14 +3,43 @@
 <head>
 	<title>Card HTML e CSS</title>
 	<link rel="stylesheet" type="text/css" href="feed.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
-<body>
-<a href="http://localhost/chat/feed/publica.php"><button>Meu Botão</button></a>
+<body >
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://previews.123rf.com/images/arbuzu/arbuzu1505/arbuzu150500003/40047867-letter-p-logo-icon-design-template-elements.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      FACEPET
+    </a>
+  </div>
+</nav>
+	
+
+
+   <div class="input-group mb-3">
+  <img class="perfil" src="https://www.imagensempng.com.br/wp-content/uploads/2021/09/Icone-usuario-Png-1024x1024.png">
+  <input type="text" class="form-control" placeholder="O que está pensando sobre seu pet hoje?" aria-label="Username" aria-describedby="basic-addon1">
+  <a href="http://localhost/chat/feed/publica.php"><button>Publicar</button></a>
+  </div>
+
+
+    </div>
+	<div class="chat"> 
+      <img id="lchat" src="http://localhost/chat/img/chat.png">
+   </div>
+   <div class="container">
 	<div class="card">
 		<img src="https://via.placeholder.com/150" alt="Imagem do Card">
 		<h3>Título do Card</h3>
 		<p>Descrição do Card</p>
-		<img id="curti" src="http://localhost/chat/img/curit.png">
+		<ul class="interacao">
+		<li><img id="curti" src="http://localhost/chat/img/curit.png"></li>
+		<li><img id="comenta" src="http://localhost/chat/img/comenta.png"></li>
+		<li><img id="compartilha" src="http://localhost/chat/img/compartilha.png"></li>
+		<ul>
 	</div>
+  </div>
+  
 </body>
 </html>
